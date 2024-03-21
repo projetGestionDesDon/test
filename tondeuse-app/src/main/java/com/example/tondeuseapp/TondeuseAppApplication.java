c6ce25dc6066a280458da4588910f38c1b3c8ff7
Mohamed FARHAOUI <mohamed.farhaoui@amadeus.com>
2024-03-21 16:32:28 +0100

    first commit

---
+++ tondeuse-app/src/main/java/com/example/tondeuseapp/TondeuseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tondeuseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TondeuseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TondeuseAppApplication.class, args);
+	}
+
+}
---
+++ tondeuse-app/src/test/java/com/example/tondeuseapp/TondeuseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tondeuseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TondeuseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
